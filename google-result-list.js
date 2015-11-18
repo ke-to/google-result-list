7b8318aa47453acf599c79d27172f7030c449477
ke-to <kuronokei0606@gmail.com>
2015-11-18 21:16:42 +0900

    本文も取得

--- google-result-list.js
+++ google-result-list.js
@@ -1,8 +1,10 @@
 a= document.getElementsByClassName('r');
+a2= document.getElementsByClassName('st');
 var c=[];
 for(var i=0;i<a.length;i++){
  b=a[i].children;
  c.push(b[0].innerHTML);
+ c.push(a2.innerHTML);
 }
 d=c.toString();
-alert(d);
+console.log(d);
